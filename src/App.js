853fa9a2749561435bc5568b35005304a2204052
chris kim <chriskim0926@gmail.com>
2021-01-30 23:44:00 -0500

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Nav />
     <Wrapper>
       <Switch>
-        <Route exact path="/" component={About} />
+        <Route exact path={["/", "/about", "*"]} component={About} />
         <Route exact path="/About" component={About} />
         <Route exact path="/Portfolio" component={Portfolio} />
         <Route exact path="/Contact" component={Contact} />
